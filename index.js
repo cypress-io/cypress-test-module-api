8ac4158ad754bf41db05812543d6f78f49f1d8f5
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-10-23 17:05:51 +0200

    add terminal banner and print all paths from results object

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
+const banner = require('terminal-banner').terminalBanner
+const allPaths = require('@bahmutov/all-paths')
 const cypress = require('cypress')
 console.log('cypress is', cypress)
+
+const onSuccess = (runResult) => {
+  banner('Cypresss results')
+  console.log('%o', runResult)
+  banner('Results paths')
+  allPaths(runResult)
+}
 
 cypress.run({
   spec: './cypress/integration/a-spec.js'
 })
-.then(console.log)
+.then(onSuccess)
 .catch(console.error)
